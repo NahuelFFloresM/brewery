d01711dd9eaae44089ec81746483be5ee661fadf
nahuelflores <arcangel6191@gmail.com>
2020-12-09 18:15:21 -0300

    Borrado de eslint-plugin-import, cambiado nombre del import en el servicio para que coincida con el archivo ts

--- src/app/services/beer-cart.service.ts
+++ src/app/services/beer-cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Beer } from '../modelos/beer';
+import { Beer } from '../modelos/Beer';
 
 @Injectable({
   providedIn: 'root'
