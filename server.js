682939abe42ba834e5fa8bc0f7c5b905797217ab
nahuelflores <arcangel6191@gmail.com>
2020-12-09 15:45:47 -0300

    Fix para que la aplicacion carge con nodejs

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/brewery'));
+app.use(express.static('./dist/brewery'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/brewery/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/brewery/'});
 });
 
 // Start the app by listening on the default Heroku port
